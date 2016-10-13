ac3c5740c9964a0015baa2724fe605320dada821
猫哥 <24251076@qq.com>
2016-10-12 23:16:02 -0400

    add exif

--- python/src/photo_organizer/exif.py
+++ python/src/photo_organizer/exif.py
@@ -2,7 +2,41 @@
 exif reader
 will accpet a filename then try to read exif data from that file
 will return dict wih exif info
+
+seems the best library I can find for exif is: https://wiki.gnome.org/Projects/gexiv2
+however I can't get it working under osx with virtulenv.
+so use http://tilloy.net/dev/pyexiv2/overview.html instead
 """
+import pyexiv2
+import fractions
+import datetime
+import time
+import os
 
 def get(filename):
-    pass
+    tags = {} 
+    if not os.access(filename, os.R_OK):
+        return tags
+    metadata = pyexiv2.ImageMetadata(filename)
+    metadata.read()
+    keys = metadata.keys()
+    for key in keys:    
+        value = None
+        tag = metadata[key]
+        try:
+            if not isinstance(tag.value, (int, str, long, unicode, fractions.Fraction, datetime.datetime)): continue
+            
+            if isinstance(tag.value, fractions.Fraction):
+                value = float(tag.value)
+            elif isinstance(tag.value, datetime.datetime):
+                value = time.mktime(tag.value.timetuple())
+            elif isinstance(tag.value, str):
+                value = tag.value.strip()
+            else:
+                value = tag.value
+            if tag.name:
+                tags.setdefault(tag.name, value)
+        except:
+            pass
+    
+    return tags
--- python/src/photo_organizer/test/test_exif.py
+++ python/src/photo_organizer/test/test_exif.py
@@ -1,6 +1,23 @@
 import unittest
 import exif
+import os
 
 class Exif(unittest.TestCase):
+    """test exif read
+    """
+    
     def test_read(self):
-        self.assertEqual(exif.get(None), None, 'exif read')
+        """test exif from example photo
+        """
+        sample = {
+            'YResolution': -0.6666666666666666,
+            'ExifTag': 90L,
+            'XResolution': -2L,
+            'DateTimeOriginal': 978325199.0,
+            'Model': 'Test 1',
+            'SamplesPerPixel': 162}
+        #pycharm running test is different than running nosetests from shell, hack the path
+        filename = os.path.join(os.path.dirname(__file__), 'exif.jpg')
+        self.assertEqual(exif.get(filename), sample, 'exif read test')        
+        self.assertEqual(exif.get('do_not_exist'), {}, 'exif read without os.R_OK')
+        
